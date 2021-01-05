8ca7f48726cb8f87daacefe89cdc7a0cf73ec736
Yash Shah <43175218+shahyash1136@users.noreply.github.com>
2021-01-05 20:05:45 +0530

    Delete FirebaseConfig.js

--- src/Config/FirebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyBHu4i9FJEtmpxkQajC43a1HpqwmqEB3Tc",
-    authDomain: "mygitapp-fa0dc.firebaseapp.com",
-    projectId: "mygitapp-fa0dc",
-    storageBucket: "mygitapp-fa0dc.appspot.com",
-    messagingSenderId: "505675147280",
-    appId: "1:505675147280:web:2725c1e9614bb0b9a2c295",
-    measurementId: "G-4LF6N9Q9TC"
-};
